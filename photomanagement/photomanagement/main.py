2c11021f2d9f8f20d8091aba2f865a8e6a0a14e1
Joseph Schwab <52177033+jschwab012@users.noreply.github.com>
2024-12-06 17:00:54 -0500

    Created the main file

---
+++ photomanagement/photomanagement/main.py
@@ -0,0 +1,15 @@
+
+from photomanagement import *
+import pathlib
+
+DB_PATH = pathlib.Path("./photo-database")
+database: Database
+
+def main():
+    db = Database(DB_PATH)
+
+    
+
+
+if __name__ == "main":
+    raise NotImplementedError
