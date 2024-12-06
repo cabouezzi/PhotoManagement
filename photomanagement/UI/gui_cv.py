01b547f0c6b043ff87022fdda9fd05798d02c16e
Anh Le <lemanhstudy4111@gmail.com>
2024-12-06 17:00:57 -0500

    fix img open bug

--- photomanagement/UI/gui_cv.py
+++ photomanagement/UI/gui_cv.py
@@ -11,12 +11,7 @@ File Handler
 """
 
 
-def choose_file_callback(sender, app_data):
-    """
-    Load image, adjust image path text, load widget groups
-    """
-    path = app_data["file_path_name"]
-    # width, height, channels, data = dpg.load_image(path)
+def load_img_init(path):
     try:
         # when there is path
         if dpg.get_value("img_path") != "Path ":
@@ -28,7 +23,7 @@ def choose_file_callback(sender, app_data):
             handle_img_obj = HandleImageDPG()
             rgba_img = handle_img_obj.cv2_open_img(path=path)
             cv2_data = handle_img_obj.texture_to_data(rgba_img)
-            dpg.add_static_texture(
+            dpg.add_raw_texture(
                 width=rgba_img.shape[1],
                 height=rgba_img.shape[0],
                 default_value=cv2_data,
@@ -38,11 +33,43 @@ def choose_file_callback(sender, app_data):
                 "texture_tag",
                 parent="img_window",
                 tag="image_tag",
-                # width=int(dpg.get_item_width("texture_tag") * 0.4),
-                # height=int(dpg.get_item_height("texture_tag") * 0.4),
             )
             dpg.set_value("img_path", value=f"Path {path}")
             create_widget_group(handle_img_obj)
+    except ValueError:
+        print("cannot load file. path does not exist")
+
+
+def choose_file_callback(sender, app_data):
+    """
+    Load image, adjust image path text, load widget groups
+    """
+    path = app_data["file_path_name"]
+    # width, height, channels, data = dpg.load_image(path)
+    handle_img_obj = HandleImageDPG()
+    rgba_img = handle_img_obj.cv2_open_img(path=path)
+    cv2_data = handle_img_obj.texture_to_data(rgba_img)
+    try:
+        # when there is path
+        if dpg.get_value("img_path") != "Path ":
+            # width, height, channels, data = dpg.load_image(path)
+            dpg.delete_item("texture_tag")
+            dpg.remove_alias("texture_tag")
+            dpg.delete_item("image_tag")
+        with dpg.texture_registry(show=True, tag="texture_registry"):
+            dpg.add_raw_texture(
+                width=rgba_img.shape[1],
+                height=rgba_img.shape[0],
+                default_value=cv2_data,
+                tag="texture_tag",
+            )
+            dpg.add_image(
+                "texture_tag",
+                parent="img_window",
+                tag="image_tag",
+            )
+        dpg.set_value("img_path", value=f"Path {path}")
+        create_widget_group(handle_img_obj)
     except ValueError:
         print("cannot load file. path does not exist")
 
